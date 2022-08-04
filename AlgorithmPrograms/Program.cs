218657227122da2b3b6a1e1a019dde50144e3e2b
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-04 20:58:22 +0530

    Add project files.

---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] words = {"Orange", "Apple", "Kiwi", "Banana", "Pineapple" };
+Console.WriteLine("Before Sorting:");
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
+InsertSort(words);
+Console.WriteLine("\nAfter Sorting:");
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
+
+void InsertSort(string[] array)
+{
+    int i, j;
+    for(i = 1; i < array.Length; i++)
+    {
+        string word = array[i];
+        j = i - 1;
+        while((j >= 0) && (array[j].CompareTo(word)>0))
+        {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j+1] = word;
+    }
+}
